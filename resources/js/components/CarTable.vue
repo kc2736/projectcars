631d6fa3762a74608c2e777b949c0e51f4958b78
kc2736 <kc2736@g.rit.edu>
2021-12-01 15:50:19 +0100

    VueJs and Vuetify setup

---
+++ resources/js/components/CarTable.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-data-table
+        :headers="headers"
+        :items="carsList"
+        :items-per-page="5"
+        class="elevation-1"
+    ></v-data-table>
+</template>
+
+<script>
+export default {
+    name: "CarTable",
+    data (){
+        return{
+            headers: [
+                {
+                    text: 'Cars',
+                    align: 'start',
+                    sortable: false,
+                    value: 'name',
+                },
+                { text: 'Type', value: 'type' },
+                { text: 'Brand', value: 'brand' },
+                { text: 'Model', value: 'model' },
+                { text: 'Year', value: 'year' },
+                { text: 'Doors', value: 'doors' },
+            ],
+            carsList:[
+                {
+                    name: 'Uno',
+                    type: 'Hatchback',
+                    brand: 159,
+                    model: 6.0,
+                    year: 24,
+                    doors: 4.0,
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
